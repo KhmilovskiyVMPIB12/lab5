29c8598dbb01021e792704b56f991a493e07056d
KhmilovskiyVMPIB12 <125645185+KhmilovskiyVMPIB12@users.noreply.github.com>
2023-06-02 18:42:51 +0700

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lab1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Regex[] r = { new Regex(@"\b[a]{1}\b"), new Regex(@"\b[a]{6}\b"), new Regex(@"\b[a]{1}\W+[a]{2}\W+[a]{1}\b") };
+			
+			string[] input = File.ReadAllLines(@"source.txt");
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				Console.WriteLine(input[i]);
+				for (int j = 0; j < r.Length; j++)
+				{
+					if (r[j].IsMatch(input[i]))
+						Console.WriteLine($"\tсоответствует шаблону \"{r[j].ToString()}\"");
+				}
+			}
+		}
+	}
+}
