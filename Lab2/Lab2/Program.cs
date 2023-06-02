b8e3d7bb81005b4f6e9c872b0924d91aa43f8066
KhmilovskiyVMPIB12 <125645185+KhmilovskiyVMPIB12@users.noreply.github.com>
2023-06-02 18:43:39 +0700

    Add files via upload

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Regex r = new Regex(@"\w{5,}");
+			string s = "";
+			while(!r.IsMatch(s))
+			{
+                Console.Write("введите не менее 5 алфавитно-цифровых символов -> ");
+                s = Console.ReadLine();
+				if (!r.IsMatch(s))
+                    Console.Write("условие не соблюдено, снова ");
+            }
+            Console.WriteLine($"Введённое значение {s} соответствует условию");
+        }
+	}
+}
