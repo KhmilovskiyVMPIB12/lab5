e2c6a8f35984dfa6392e9b3ef46a01bf7f2eaeb9
KhmilovskiyVMPIB12 <125645185+KhmilovskiyVMPIB12@users.noreply.github.com>
2023-06-02 18:47:51 +0700

    Add files via upload

---
+++ Addition1/Addition1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Addition1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string DecimalSeparator = 0.0m.ToString().Replace("0", "");
+			string t = "Добро пожаловать в наш магазин, вот наши цены: 1 кг. яблоки - 90 руб., 2 кг. апельсины - 130 руб. Также в ассортименте орехи в следующей фасовке: 0.5 кг. миндаль - 500 руб.";
+            Console.WriteLine(t);
+
+			Regex r = new Regex(@"(?<weight>\d+[\.,]*\d*)\s+кг\.\s+(?<product>[\w\s]+)\s*-\s*(?<cost>\d+[\.,]*\d*)\s+руб\.");
+			Match m = r.Match(t);
+			while (m.Success)
+			{
+				float cost = float.Parse(m.Groups["cost"].Value);
+				float weight = float.Parse(m.Groups["weight"].Value.Replace(".", DecimalSeparator));
+                Console.WriteLine($"{m.Groups["product"]} - {cost / weight}");
+                m = m.NextMatch();
+			}
+        }
+	}
+}
---
+++ Addition2/Addition2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Addition2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Regex r = new Regex(@"\b([fhtps]*:)?/{0,2}[wW]{0,3}\.*([a-zA-z0-9_][a-zA-z0-9_\-]*\.)*[a-zA-z0-9_][a-zA-z0-9_\-]+\.[a-zA-z]+\b");
+			Regex r1 = new Regex(@"^(([a-zA-Z][0-9a-zA-Z+\-\.]*:)?/{0,2}[0-9a-zA-Z;/?:@&=+$\.\-_!~*'()%]+)?(#[0-9a-zA-Z;/?:@&=+$\.\-_!~*'()%]+)?");
+
+			string[] input = File.ReadAllLines(@"source.txt");
+
+			using (StreamWriter writer = new StreamWriter("result.txt", false))
+			{
+				for (int i = 0; i < input.Length; i++)
+				{
+					Match m = r.Match(input[i]);
+
+					while (m.Success)
+					{
+						writer.WriteLine($"подходящее под шаблон значение {m.Value} расположено в позиции {m.Index} строки {i + 1}");
+						m = m.NextMatch();
+					}
+				}
+			}
+		}
+	}
+}
