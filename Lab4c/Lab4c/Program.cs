0b033a0784603e0ac324c09e412432ecb963a9c5
KhmilovskiyVMPIB12 <125645185+KhmilovskiyVMPIB12@users.noreply.github.com>
2023-06-02 18:46:20 +0700

    Add files via upload

---
+++ Lab4c/Lab4c/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab4c
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			string info;
+
+			Regex r = new Regex(@"\b(?<f>[\w]{2,})\b\s*\b(?<i>[\w]{2,})\b\s*\b(?<o>[\w]*)\b\s*,\s*(?<age>[1-9]{1}\d+)\s*лет,\s*(г\.)?\s*(?<city>\S{2,})");
+			
+			while(true)
+			{
+				Console.Write("Введите данные пользователя, или пустую строку для выхода -> ");
+				info = Console.ReadLine();
+				if (info.Length == 0) break;
+
+				Match match = r.Match(info);
+				if (match.Success)
+				{
+					Console.WriteLine($"город: {match.Groups["city"]}");
+					Console.WriteLine($"имя: {match.Groups["f"]} {match.Groups["i"]} {match.Groups["o"]}");
+					Console.WriteLine($"возраст: {match.Groups["age"]}");
+				}
+				else
+					Console.WriteLine("Не найдено");
+
+			}
+		}
+	}
+}
